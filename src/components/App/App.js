96537e45cf2fe536bc05813c6151a91ee554dafb
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-09-16 18:06:47 +1000

    Add search bar and search button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
 export default function App() {
-  return <div>Hello World</div>
+  return (
+    <div className="cf">
+      <label htmlFor="city" className="f6 b db mb2">
+        <span className="normal black-60">Search for a city</span>
+      </label>
+      <input
+        id="city"
+        className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
+        placeholder="Enter a city or a station name"
+        type="text"
+      />
+      <button className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns">
+        Search
+      </button>
+      <small className="f6 black-60 db mb2">
+        You can search by the suburb or station name
+      </small>
+    </div>
+  )
 }
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,9 +3,31 @@ import App from './App'
 import { render, screen } from '@testing-library/react'
 
 describe('<App/>', () => {
-  it('renders', () => {
-    render(<App />)
+  describe('searching for a location', () => {
+    function subject() {
+      render(<App />)
 
-    expect(screen.getByText('Hello World')).toBeInTheDocument()
+      const searchField = screen.getByRole('textbox', {
+        name: 'Search for a city',
+      })
+
+      const searchButton = screen.getByRole('button', {
+        name: 'Search',
+      })
+
+      return { searchField, searchButton }
+    }
+
+    it('has a search bar', () => {
+      const { searchField } = subject()
+
+      expect(searchField).toBeInTheDocument()
+    })
+
+    it('has a search button', () => {
+      const { searchButton } = subject()
+
+      expect(searchButton).toBeInTheDocument()
+    })
   })
 })
