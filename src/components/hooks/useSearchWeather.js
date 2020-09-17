7ace7359d7fed0f22df2f16beff1b24e892d90d5
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-09-17 11:19:34 +1000

    complete useWeatherHook

--- src/components/hooks/useSearchWeather.js
+++ src/components/hooks/useSearchWeather.js
@@ -1,3 +1,64 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import axios from 'axios'
 
-export default function useSearchWeather() {}
+export function searchWeatherUrl(stationName) {
+  return `http://api.waqi.info/feed/${name}/?token=8d8e978e647d2b0a8c17c04ba331c0117cd06dc8`
+}
+
+const initState = {
+  success: null,
+  loading: false,
+  error: '',
+  feed: null,
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'loading':
+      return { ...state, loading: true }
+    case 'success':
+      return {
+        ...state,
+        feed: action.feed,
+        loading: false,
+        success: true,
+      }
+    case 'error':
+      return {
+        ...state,
+        error: 'Something went wrong. Please try again',
+        loading: false,
+        success: false,
+      }
+  }
+}
+
+function serializeFeed(feed) {
+  const { city, aqi } = feed
+
+  return {
+    cityName: city.name,
+    geoLocation: { lat: city.geo[0], lng: city.geo[1] },
+    url: city.url,
+    aqi: aqi,
+  }
+}
+
+export default function useSearchStation() {
+  const [state, dispatch] = useReducer(reducer, initState)
+
+  const searchWeather = async (stationName) => {
+    dispatch({ type: 'loading' })
+
+    try {
+      const res = await axios.get(searchWeatherUrl(stationName))
+      const feed = serializeFeed(res.data)
+
+      dispatch({ type: 'success', feed })
+    } catch (e) {
+      dispatch({ type: 'error' })
+    }
+  }
+
+  return { searchWeather, ...state }
+}
---
+++ src/components/hooks/useSearchWeather.test.js
@@ -0,0 +1,209 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { renderHook, act } from '@testing-library/react-hooks'
+
+import useSearchWeather, { searchWeatherUrl } from './useSearchWeather'
+
+const server = setupServer()
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+describe('useSearchStation hook', () => {
+  function subject(stationName = '', data = {}, status = '200') {
+    const url = searchWeatherUrl(stationName)
+    server.use(
+      rest.get(url, (_req, res, ctx) => {
+        return res(ctx.status(status), ctx.json(data))
+      })
+    )
+
+    return renderHook(() => useSearchWeather())
+  }
+
+  describe('initial states', () => {
+    it('sets success as null', () => {
+      const { result } = subject()
+
+      expect(result.current.success).toBeNull()
+    })
+
+    it('sets feed as null', () => {
+      const { result } = subject()
+
+      expect(result.current.feed).toBeNull()
+    })
+
+    it('sets loading as false', () => {
+      const { result } = subject()
+
+      expect(result.current.loading).toBeFalsy()
+    })
+
+    it('sets error as null', () => {
+      const { result } = subject()
+
+      expect(result.current.error).toBe('')
+    })
+  })
+
+  describe('while searching', () => {
+    it('sets success as null', async () => {
+      const { result, waitForNextUpdate } = subject()
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      expect(result.current.success).toBeNull()
+      await waitForNextUpdate()
+    })
+
+    it('sets feed as null', async () => {
+      const { result, waitForNextUpdate } = subject()
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      expect(result.current.feed).toBeNull()
+      await waitForNextUpdate()
+    })
+
+    it('sets loading as true', async () => {
+      const { result, waitFor, waitForNextUpdate } = subject()
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+
+      await waitFor(() => {
+        expect(result.current.loading).toBeTruthy()
+      })
+      await waitForNextUpdate()
+    })
+
+    it('sets error as null', async () => {
+      const { result, waitForNextUpdate } = subject()
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      expect(result.current.error).toBe('')
+      await waitForNextUpdate()
+    })
+  })
+
+  describe('successful search', () => {
+    const feedData = {
+      aqi: 71,
+      city: {
+        name: 'Melbourne, CBD',
+        url: 'ww.url1.com',
+        geo: ['2', '3'],
+      },
+      attributions: [
+        {
+          url: 'ww.url2.com',
+          name: 'something',
+        },
+      ],
+    }
+
+    it('sets success to true', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', feedData)
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.success).toBeTruthy()
+    })
+
+    it('sets feed', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', feedData)
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.feed).toEqual({
+        cityName: 'Melbourne, CBD',
+        geoLocation: { lat: '2', lng: '3' },
+        url: 'ww.url1.com',
+        aqi: 71,
+      })
+    })
+
+    it('sets loading to false', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', feedData)
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.loading).toBeFalsy()
+    })
+
+    it('does not set error', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', feedData)
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.error).toBe('')
+    })
+  })
+
+  describe('failed search', () => {
+    it('sets success to false', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', {}, '500')
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.success).toBeFalsy()
+    })
+
+    it('sets feed as null', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', '500')
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.feed).toBeNull()
+    })
+
+    it('sets loading to false', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', {}, '500')
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.loading).toBeFalsy()
+    })
+
+    it('sets error message', async () => {
+      const { result, waitForNextUpdate } = subject('Melbourne', {}, '500')
+
+      act(() => {
+        result.current.searchWeather('Melbourne')
+      })
+      await waitForNextUpdate()
+
+      expect(result.current.error).toBe(
+        'Something went wrong. Please try again'
+      )
+    })
+  })
+})
